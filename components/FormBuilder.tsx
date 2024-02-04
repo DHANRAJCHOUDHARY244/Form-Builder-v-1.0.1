4023f7fcfbef494f88d3519ec87f9fd81f7fb08a
DHANRAJCHOUDHARY244 <choudharydhanraj239@gmail.com>
2024-02-05 00:22:50 +0530

    new modules add

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -4,30 +4,38 @@ import React from 'react'
 import PreviewDialogBtn from './PreviewDialogBtn'
 import SaveFormBtn from './SaveFormBtn'
 import PublishFormBtn from './PublishFormBtn'
+import Designer from './Designer'
+import { DndContext } from '@dnd-kit/core'
 
-const FormBuilder = ({form}:{
-    form:Form
+const FormBuilder = ({ form }: {
+    form: Form
 }) => {
-  return (
-    <main className="flex flex-col w-full" >
-        <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-            <h2 className='truncate font-medium' >
-                <span className='text-muted-foreground mr-2'>Form:</span>
-                {form.name}
-            </h2>
-            <div className="flex items-center bgap-2">
-                <PreviewDialogBtn/>
-                {!form.published &&(
-                    <>
-                    <SaveFormBtn/>
-                    <PublishFormBtn/>
-                    </>
-                )}
-            </div>
-        </nav>
-        <div className=""></div>
-    </main>
-  )
+    return (
+        <DndContext>
+            <main className="flex flex-col w-full" >
+                <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+                    <h2 className='truncate font-medium' >
+                        <span className='text-muted-foreground mr-2'>Form:</span>
+                        {form.name}
+                    </h2>
+                    <div className="flex items-center gap-2">
+                        <PreviewDialogBtn />
+                        {!form.published && (
+                            <>
+                                <SaveFormBtn />
+                                <PublishFormBtn />
+                            </>
+                        )}
+                    </div>
+                </nav>
+                <div className="flex w-full flex-grow 
+         items-center justify-center relative 
+         overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+                    <Designer />
+                </div>
+            </main>
+        </DndContext>
+    )
 }
 
 export default FormBuilder
---
+++ components/FormElements.tsx
@@ -0,0 +1,1 @@
+export type ElementType='TextField'
